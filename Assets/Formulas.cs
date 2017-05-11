49bbbb2e93526def8c61b8e97acea1e02ed25322
Russ Perlow <rep4975@rit.edu>
2017-05-11 12:57:45 -0400

    Formulas to GUI added

---
+++ Assets/Formulas.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formulas : MonoBehaviour {
+
+	// Public Variables
+	public GameObject newestProjectile;
+
+	// Private Variables
+	private Vector3 velocity = Vector3.zero;
+	private float gravity = 9.81f;
+	private float angle;
+	private float range;
+	private float height;
+	private GameObject cannon;
+	private GameObject[] gameObjects;
+
+	// Use this for initialization
+	void Start () {
+		gameObjects = GameObject.FindGameObjectsWithTag ("Projectile");
+		cannon = GameObject.FindGameObjectWithTag ("Cannon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Only use the newest projectile's information
+		gameObjects = GameObject.FindGameObjectsWithTag ("Projectile");
+		if (gameObjects.Length > 0) {
+			newestProjectile = gameObjects [gameObjects.Length - 1];
+			// Debug.Log ("Name: " + newestProjectile.name);
+
+
+			velocity = new Vector3 (0, 0, 30);
+
+			// Calculate the final position
+			angle = 90 - cannon.transform.eulerAngles.x;
+			//Debug.Log ("Angle: " + angle + " Cannon: " + cannon.transform.eulerAngles.x);
+			//Debug.Log ("Sin: " + Mathf.Sin((2 * angle) * (Mathf.PI / 180)));
+			range = (30 * 30 * (Mathf.Sin((2 * angle) * (Mathf.PI / 180)))) / gravity;
+
+			// Calculate the max height
+			height = (30 * 30 * (Mathf.Sin((angle) * (Mathf.PI / 180))) * Mathf.Sin((angle) * (Mathf.PI / 180))) /  (2 * gravity);
+		}
+	}
+
+	void OnGUI(){
+		GUI.color = Color.cyan;
+		GUI.Label (new Rect (0, 0, 400, 200), "Final Horizontal Position: " + range);
+		GUI.Label (new Rect (0, 25, 200, 200), "Max Height: " + height);
+		// GUI.Label (new Rect (0, 50, 200, 200), "Current Position: ");
+	}
+}
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -5,46 +5,12 @@ using UnityEngine;
 public class Path : MonoBehaviour {
 
 	// Public Variables
-	public Material mat;
-    public GameObject cannon;
-
-	// Private Variables
-	private float y;	// Vertical position
-	private float y0;	// Initial vertical position
-	private float x;	// Horizontal position
-	private float x0;	// Initial horizontal position
-	private float v0;	// Initial velocity
-	private float velocity;	// Velocity used for calculations
-	// private float gravity = -9.81f; // Acceleration due to gravity
-	private float angle;	// Angle of object
-	private float t = .01f;	// Time used for calculations
-    private float duration; // How long the flight is
-    private float distance; // Final distance of this projectile
-    private List<Vector2> pathLines;
+	public Vector3 v0 = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-        /*
-        cannon = GameObject.FindGameObjectWithTag("Cannon");
-		y0 = gameObject.transform.position.y;
-		x0 = gameObject.transform.position.z;
-        Launch launch = (Launch)cannon.GetComponentInChildren(typeof(Launch));
-        v0 = launch.force; 
-		angle = cannon.transform.rotation.x;
-        angle = angle * 180 / Mathf.PI;
-
-        distance = (v0 * Mathf.Cos(angle) / g) * ((v0 * Mathf.Sin(angle)) + Mathf.Sqrt((v0 * v0 * Mathf.Sin(angle)) + 2 * g * y0));
-        duration = (2 * v0 * Mathf.Sin(angle)) / g;
-        Debug.Log("Duration: " + duration);
-        // Debug.Break();
-        // t = duration / 100;
-
-        pathLines = new List<Vector2>();
-        */
+		v0 = GetComponent<Rigidbody>().velocity;
         GetPath(transform.position, GetComponent<Rigidbody>().velocity, new Vector3(0, -9.81f, 0));
-
-        // Debug.Log("Angle: " + angle + " Rotation: " + cannon.transform.rotation.x);
-        
 	}
 	
 	// Update is called once per frame
@@ -52,8 +18,7 @@ public class Path : MonoBehaviour {
 		
 	}
 		
-    void GetPath(Vector3 initialPosition, Vector3 initialVelocity, Vector3 gravity)
-    {
+    void GetPath(Vector3 initialPosition, Vector3 initialVelocity, Vector3 gravity) {
         int numSteps = 1000; // for example
         float timeDelta = 1.0f / initialVelocity.magnitude; // for example
 
@@ -69,40 +34,8 @@ public class Path : MonoBehaviour {
             position += velocity * timeDelta + 0.5f * gravity * timeDelta * timeDelta;
             velocity += gravity * timeDelta;
 
-            if (position.y < 0)
+            if (this.transform.position.y < 1)
                 break;
         }
-
-        /*
-        Debug.Log("Distance: " + distance);
-		do 
-		{
-            Debug.Log("X: " + x + " Y: " + y + " Angle: " + angle + " Time: " + t + " V0: " + v0);
-            
-            x = v0 * t;
-            y = Mathf.Tan(angle) * x - ((g / (2 * v0 * v0 * (Mathf.Cos(angle) * Mathf.Cos(angle)))) * (x * x));
-
-            // Add this position to the path
-            pathLines.Add(new Vector2(x, y));
-
-			// Increase the time
-			t += duration / 100;
-        } while (y >= 0);
-        */
     }
-
-	void OnRenderObject()
-	{
-        /*
-        mat.SetPass(0);
-        GL.Begin(GL.LINES);
-
-        for(int i = 0; i < pathLines.Count; i++)
-        {
-            GL.Vertex(pathLines[i]);
-        }
-
-        GL.End();
-        */
-	}
 }
